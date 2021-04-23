08d09be9ee3bf52a2ac7c5975585d34e7e4bde45
264719_PawanSingh <kishore.pawansingh@gmail.com>
2021-04-23 12:31:54 +0530

    merging dictionaries

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+from mergedict import *
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+
+if __name__ == '__main__':
+    print_hi('Python \nReady to code...')
+    inputdictionary()
+    merge_dictionary()
+    print_result(result)
+
---
+++ mergedict.py
@@ -0,0 +1,18 @@
+dict1={}
+dict2={}
+result={}
+def inputdictionary():
+    print("Enter the values in dictionary 1")
+    n = 3
+    dict1 = [ map(str,input().split()) for x in range(n)]
+    print("Enter the values in dictionary 2")
+    dict2 = [ map(str,input().split()) for x in range(n)]
+    return
+
+def merge_dictionary():
+    result={**dict1, **dict2}
+    return result
+
+def print_result(result):
+    print("After merging the Dictionary")
+    print(result)
