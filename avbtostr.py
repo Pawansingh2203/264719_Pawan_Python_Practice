0b359950e1a804c453a911273a620f3e49a49494
264719_PawanSingh <kishore.pawansingh@gmail.com>
2021-04-28 12:41:24 +0530

    6th commit. Abbreviation to String function added.

---
+++ avbtostr.py
@@ -0,0 +1,13 @@
+def avbtostr():
+    print("Enter the abbreviation")
+    s=input()
+    if s=='lol':
+        print("Laughing out Loud")
+    elif s=='rofl':
+        print("Rolling on floor laughing")
+    elif s=='lmk':
+        print("Let me Know")
+    elif s=='smh':
+        print("Shaking my head")
+    else:
+        print("Wrong Choice !")
